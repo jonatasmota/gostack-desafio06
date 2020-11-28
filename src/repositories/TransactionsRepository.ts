b9c4da319c7b00dd763930720205c6b23079aa3a
jonatasmota <jonatascmota@gmail.com>
2020-11-28 02:48:27 -0300

    Desafio 06 Concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,29 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const { sum: incomeSum } = await this.createQueryBuilder()
+      .select('SUM(value)')
+      .where({
+        type: 'income',
+      })
+      .getRawOne();
+
+    const { sum: outcomeSum } = await this.createQueryBuilder()
+      .select('SUM(value)')
+      .where({
+        type: 'outcome',
+      })
+      .getRawOne();
+
+    const income: number = parseFloat(incomeSum) || 0.0;
+    const outcome: number = parseFloat(outcomeSum) || 0.0;
+    const total: number = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 }
 
